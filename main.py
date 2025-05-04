cc98cf0fc2bf4c1e9500df9b5bc4d9ad4d1d5517
JakobArnfred <jakob@scan-soft.dk>
2025-05-04 22:30:50 +0200

    latest

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ ti.init(arch=ti.gpu, random_seed=int(time.time()))
 width = 1200
 height = 800
 ratio = width/height
-amount = 200
+amount = 20
 types = 2
-rad = 1
+rad = 2
 max_speed = 0.005
-sigma = 1 * rad / width
-epsilon = 0.1
+sigma = 6 * rad / width
+epsilon = 0.01
 boxHeight = 0.4
 boxWidth = 0.35
 foodAmount = 20
@@ -43,10 +43,10 @@ def init():
     for k in range(foodAmount):
         positions[1, k] = [ti.random()*0.25 + 0.75, ti.random()*0.5 + boxHeight+0.05]
         velocities[1, k] = [0.0, 0.0]
-    forces[0, 0] = 1
+    forces[0, 0] = -1
     forces[0, 1] = 0
     forces[1, 0] = 0
-    forces[1, 1] = 1
+    forces[1, 1] = -1
         
     
     # forces[0, 0] = 0.5
@@ -64,27 +64,44 @@ def update_vel():
 
         for iOther, jOther in ti.ndrange(types, amount):
             if i != iOther or j != jOther:
-                # force = forces[i, iOther]
-                dir = positions[iOther, jOther] - pos1
-                dist_sqr = dir.norm_sqr() + 1e-10
-                dist = ti.sqrt(dist_sqr)
+                dir = positions[iOther, jOther] - pos1  # Vector from current to other particle 
+                dist_sqr = dir.norm_sqr() + 1e-10       # 
+                dist = ti.sqrt(dist_sqr)                # Distance between particles
+                r = dist                                # Distance to be used for function
+                # print(dir, dist_sqr, dist)
                 
-                repulsive_force = (sigma**12) / (dist_sqr**6)
-                attraction_force = (sigma**6) / (dist_sqr**3)
+                if (dist > 4*sigma):
+                    r = ti.sqrt(ti.sqrt(dist))
+                #     # print((4*sigma), dist)
+                
+                # repulsive_force = (sigma**12) / (dist_sqr**6)
+                # attraction_force = (sigma**6) / (dist_sqr**3)
+                
+                m = 6.0
+                n = 4.0
+                temp = sigma / r
+                repulsive_force = temp ** m
+                attraction_force = temp ** n
+                
+                
+                rep_f = (sigma / dist)
                 
                 force_Mult = forces[i, iOther]
                 newEpsilon = epsilon * force_Mult
+                
+                result_force = newEpsilon * (m * repulsive_force - n * attraction_force) / r
                 
                 # if force_Mult < 0 and dist <= sigma:
                 #     newEpsilon = epsilon * force_Mult
                 # else:
                 #     newEpsilon = epsilon * force_Mult
 
-                result_force = 24 * newEpsilon * (2 * repulsive_force - attraction_force) / dist
+                # result_force = 24 * newEpsilon * (2 * repulsive_force - attraction_force) / dist
                 endForce = result_force * dir.normalized()
-                
-                if force_Mult < 0:
-                    endForce = -abs(result_force) * dir.normalized()
+                # print("hhh")
+                # if force_Mult < 0:
+                #     print("ggg")
+                #     endForce = -abs(result_force) * dir.normalized()
                     
                 # if (endForce > 0):
                 #     endForce *= 10
@@ -102,6 +119,8 @@ def update_vel():
         speed = velocities[i, j].norm()
         if speed > max_speed:
             velocities[i, j] = velocities[i, j].normalized() * max_speed
+        else:
+            velocities[i, j] = velocities[i, j].normalized() * speed*0.99
 
 @ti.kernel
 def check_coll():
